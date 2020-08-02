94504f9c7b4081bc92d8584731195fc2c62d8679
Zarak <7254237+zarak@users.noreply.github.com>
2020-08-02 08:25:34 +0500

    Initial simple functions

--- chapter2/src/Chapter2/SimpleFunctions.hs
+++ chapter2/src/Chapter2/SimpleFunctions.hs
@@ -1,3 +1,28 @@
 module Chapter2.SimpleFunctions where
 
+
+firstOrEmpty :: [String] -> String
 firstOrEmpty lst = if not (null lst) then head lst else "empty"
+
+
+(+++) :: [a] -> [a] -> [a]
+lst1 +++ lst2 = if null lst1
+                   then lst2
+                   else (head lst1) : (tail lst1 +++ lst2)
+
+
+reverse2 :: [String] -> [String]
+reverse2 list = if null list
+                then []
+                else reverse2 (tail list) +++ [head list]
+
+
+maxmin :: Ord a => [a] -> (a, a)
+maxmin list = let h = head list
+              in if null (tail list)
+                 then (h, h)
+                 else ( if h > t_max then h else t_max
+                      , if h < t_min then h else t_min )
+                      where t = maxmin (tail list)
+                            t_max = fst t
+                            t_min = snd t
